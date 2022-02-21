9d761b4057573e6791d5dbe5d9ede46b98387e5e
Kevin Gomes <kevinmg50@gmail.com>
2022-02-21 11:31:52 -0300

    feat(user): mecanismo de cadastro criado

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,24 @@
-export class CreateUserDto {}
+import { User } from '../entities/user.entity';
+import {
+  IsEmail,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class CreateUserDto extends User {
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(6)
+  @MaxLength(30)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'Senha muito fraca!',
+  })
+  password: string;
+
+  @IsString()
+  name: string;
+}
