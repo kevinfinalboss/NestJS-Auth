3f1fc4ca267b914f9d49eb61a89ab3320e32bd59
Kevin Gomes <kevinmg50@gmail.com>
2022-02-22 01:27:36 -0300

    feat(swagger): iniciado implementação das rotas

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const config = new DocumentBuilder()
+    .setTitle('Documentação com Swagger - Login JWT com NestJS')
+    .setDescription(
+      'Documentação completa do projeto de autenticação jwt usando NestJS.',
+    )
+    .setVersion('1.0')
+    .addTag('users')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
